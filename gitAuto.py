a90d7198441e13b988059ffaadf38cc538cdd674
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2022-08-12 22:57:30 +0530

    Crucial Commit

--- gitAuto.py
+++ gitAuto.py
@@ -1,6 +1,6 @@
 import os
 
-lst = ['git pull','git add -A','git commit -m "First Commit"','git push']
+lst = ['git pull','git add -A','git commit -m "Crucial Commit"','git push']
 
 for i in lst:
     os.system(i)
